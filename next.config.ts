20dcf9bf69e2fbbaaf0e3933f45c21a75a9c9aeb
asgard <awesomepyc@outlook.com>
2025-11-25 11:47:07 +0800

    try fix prisma 1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingIncludes: {
+    // Include your custom Prisma client output folder
+    // Adjust the path to match your real structure
+    "/*": ["./app/generated/prisma/**/*"],
+    "/api/*": ["./app/generated/prisma/**/*"],
+  },
 };
 
 export default nextConfig;
